01bb0f94690736ac7f3b5fd05750c71591ded829
Axel Husson <axel.husson@gmail.com>
2023-03-05 19:08:27 +0100

    ajout docstring

--- Tuto/main.py
+++ Tuto/main.py
@@ -15,7 +15,7 @@ def print_hi(name):
     :doc-author: Trelent
     """
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}, how are you?')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
