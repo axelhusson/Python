e773a51e4da9d7c8700b2d0a0754753e14d7ec1f
Axel Husson <axel.husson@gmail.com>
2023-03-06 00:30:38 +0100

    correction useBDD

--- useBDD/main.py
+++ useBDD/main.py
@@ -4,13 +4,15 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import sqlite3
+
+import connexion as connexion
 from sqlalchemy import Column, Integer, Unicode, UnicodeText, String
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from random import choice
-from string import letters
+#from string import letters
 
 
 engine = create_engine('sqlite:database.db', echo=True)
@@ -21,7 +23,7 @@ class Player(Base):
     id = Column(Integer, primary_key=True)
     name = Column(Unicode(40))
     level = Column(Integer)
-    classe = Column(Strind(20))
+    classe = Column(String(20))
 
     def __init__(self, name, level=None, classe=None):
         self.name = name
@@ -33,12 +35,6 @@ Base.metadata.create_all()
 Session = sessionmaker(bind=engine)
 s = Session()
 
-u = Player(Axel)
-
-
-
-
-
-
+#u = Player(Axel)
 
 connexion.close()
